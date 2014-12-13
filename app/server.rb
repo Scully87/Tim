b7560db777f9206056b6d7d87142d8e5e78df026
Scully87 <scully87.ts@googlemail.com>
2014-12-13 19:33:45 +0000

    changing fade in times for footer, added next 3 pages with background colors

--- app/server.rb
+++ app/server.rb
@@ -7,3 +7,15 @@ set :public_folder, Proc.new { File.join(root, '..', 'public') }
 get '/' do
 	erb :index
 end
+
+get '/about' do
+	erb :about
+end
+
+get '/portfolio' do
+	erb :portfolio
+end
+
+get '/contact' do
+	erb :contact
+end
